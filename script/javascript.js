e0354a492a696fcb261bdb5952c9fdaffc451d16
ConnorAGorry <cgorry@ucsd.edu>
2018-11-06 17:13:11 -0800

    Connor 5:12
    active groups container. started a members array.

---
+++ script/javascript.js
@@ -0,0 +1,5 @@
+var members = [
+    {"name": "Harry Potter", "profilePic": "https://image.afcdn.com/story/20180903/trois-nouveaux-livres-harry-potter-1298096_w767h767c1cx252cy106.jpg", "index": 1}
+    
+    
+]
