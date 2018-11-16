d64e0f6988dea4e71c4d80aa03fa7959a368215c
ConnorAGorry <cgorry@ucsd.edu>
2018-11-16 00:27:07 -0800

    --

--- script/javascript.js
+++ script/javascript.js
@@ -1,10 +1,9 @@
 var member = [
-    {"name": "Harry Potter", "profilePic": "https://image.afcdn.com/story/20180903/trois-nouveaux-livres-harry-potter-1298096_w767h767c1cx252cy106.jpg", "index": 1} 
-    
+{"name": "Harry Potter", "profilePic": "https://image.afcdn.com/story/20180903/trois-nouveaux-livres-harry-potter-1298096_w767h767c1cx252cy106.jpg", "index": 1} 
+
 ]
 
 var meetup = [
-	{"location":"Westfield UTC", "time":"12:00PM", "Comment":"What should we eat when we get there?"},
 
 ]
 
@@ -19,42 +18,72 @@ var complexData = [
   {'title': 'Social design', 'profilePic': 'images/lorempixel.people.2.jpeg', 'index': 6},
   {'title': 'Gestural interaction', 'profilePic': 'images/lorempixel.technics.2.jpeg', 'index': 7},
   {'title': 'Design tools', 'profilePic': 'images/lorempixel.city.2.jpeg', 'index': 8}
-]*/
+  ]*/
 
 
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
   console.log('hello world');
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+  var parentDiv = $("#templatedProjects");
+  var meetupArray = JSON.parse(localStorage.getItem('meetupArray'));
+  for(var i = 0; i<meetupArray.length;i++){
+    console.log(localStorage.getItem('meetupArray[i].split'));
+    if(localStorage.getItem('meetupArray[i].split' == "true")){
+      source = $("#split-entry-template").html();
+      template = Handlebars.compile(source);
+      parentDiv = $("#templatedProjects");
+      meetupArray = JSON.parse(localStorage.getItem('meetupArray'));
+      var curData = meetupArray[i];
+      var curHtml = template(curData);
+      parentDiv.append(curHtml);
+    }
+    else{
+      var curData = meetupArray[i];
+      var curHtml = template(curData);
+      parentDiv.append(curHtml);
+    }
+  }
+
   // compile the template
   // BEGIN - STEP 1
-  });
+});
 
-var submit = document.getElementById("suggest");
-submit.onclick = function(){
+var suggest = document.getElementById("suggest");
+suggest.onclick = function(){
   var location = document.getElementById("location-name").value;
 //  localStorage.setItem('newLocation',location);
-  var time = document.getElementById("time-input").value;
+var time = document.getElementById("time-input").value;
   //localStorage.setItem('newTime',time);
   var comment = document.getElementById("message-text").value;
   //localStorage.setItem('newComment',comment);
 
-
-  /*var locationOutput = localStorage.getItem('newLocation');
-  $("#location-name").html(locationOutput);
-  var timeOutput = localStorage.getItem('newTime');
-  $("#time-input").html(timeOutput);
-  var commentOutput = localStorage.getItem('newComment');
-  $("#message-text").html(commentOutput);*/
-  //document.getElementById("outputTime").innerText;
-  //document.getElementById("outputComment").innerText = comment;
+  location.value = localStorage.getItem('location-name');
+  time.value = localStorage.getItem('time-input');
+  comment.value = localStorage.getItem('message-text');
+  
+  localStorage.setItem('location-name',location.value);
+  localStorage.setItem('time-input',time.value);
+  localStorage.setItem('message-text',comment.value);
+  
 
   var suggestion={"location":location,"time":time,"comment":comment};
-  var source = $("#entry-template").html();
-  var template = Handlebars.compile(source);
-  var html = template(suggestion);
-  var parentDiv = $("#templatedProjects");
-  parentDiv.append(html);
+  console.log(suggestion);
+  //var suggestionObj = JSON.parse(suggestion);
+  //var html = template(suggestion);
+  //var parentDiv = $("#templatedProjects");
+  //parentDiv.append(html);
+  localStorage.setItem("newSuggestion",JSON.stringify(suggestion));
+  var existingItems = JSON.parse(localStorage.getItem('meetupArray'));
+  if(existingItems == null){
+    existingItems = [];
+  }
+  existingItems.push(suggestion);
+  localStorage.setItem('meetupArray',JSON.stringify(existingItems));
+
+  window.location.reload();
    /* $('.modal').removeClass('in');
    $('.modal').css("display", "none");
    */
@@ -69,34 +98,62 @@ submit.onclick = function(){
 
 var splitSuggest = document.getElementById("splitsuggest");
 splitSuggest.onclick = function(){
-  var location = document.getElementById("location-name").value;
+  var location = document.getElementById("split-location-name").value;
 //  localStorage.setItem('newLocation',location);
-  var time = document.getElementById("time-input").value;
+var time = document.getElementById("time-input").value;
   //localStorage.setItem('newTime',time);
-  var comment = document.getElementById("message-text").value;
+  var comment = document.getElementById("split-message-text").value;
   //localStorage.setItem('newComment',comment);
-  var suggestion={"location":location,"time":time,"comment":comment};
-  var source = $("#entry-template").html();
-  var template = Handlebars.compile(source);
-  var html = template(suggestion);
-  var parentDiv = $("#templatedProjects");
-  var currDiv = parentDiv.append(html);
+  var split = "1";
+  location.value = localStorage.getItem('location-name');
+  time.value = localStorage.getItem('time-input');
+  comment.value = localStorage.getItem('message-text');
+
+  localStorage.setItem('location-name',location.value);
+  localStorage.setItem('message-text',comment.value);
+  localStorage.setItem('split',split.value);
+  //localStorage.setItem('split',true);
+  
+  var suggestion={"location":location,"time":time,"comment":comment,"split":split};
+  //console.log(suggestion);
+  //var suggestionObj = JSON.parse(suggestion);
+  //var html = template(suggestion);
+  //var parentDiv = $("#templatedProjects");
+  //parentDiv.append(html);
+  localStorage.setItem("newSuggestion",JSON.stringify(suggestion));
+  console.log(localStorage.getItem("newSuggestion"));
+  var existingItems = JSON.parse(localStorage.getItem('meetupArray'));
+  if(existingItems == null){
+    existingItems = [];
+  }
+  existingItems.push(suggestion);
+  localStorage.setItem('meetupArray',JSON.stringify(existingItems));
+
+  window.location.reload();
 }
 
 var edit = document.getElementById("saveEdits");
 edit.onclick = function(){
-  var location = document.getElementById("location-name").value;
+  localStorage.getItem('meetupArray')
+  var location = document.getElementById("split-location-name").value;
   //localStorage.setItem('newLocation',location);
   var time = document.getElementById("time-input").value;
   //localStorage.setItem('newTime',time);
-  var comment = document.getElementById("message-text").value;
+  var comment = document.getElementById("split-message-text").value;
   //localStorage.setItem('newComment',comment);
-  var suggestion={"location":location,"time":time,"comment":comment};
-  var source = $("#entry-template").html();
 
-  var template = Handlebars.compile(source);
-  var edited = template(suggestion);
-  var parentDiv = $("#templatedProjects");
+  location.value = localStorage.getItem('location-name');
+  time.value = localStorage.getItem('time-input');
+  comment.value = localStorage.getItem('message-text');
+
+  localStorage.setItem('location-name',location.value);
+  localStorage.setItem('time-input',time.value);
+  localStorage.setItem('message-text',comment.value);
+
+  var suggestion={"location":location,"time":time,"comment":comment};
+  localStorage.setItem("newSuggestion",JSON.stringify(suggestion));
+
+  console.log()
 }
 
 
@@ -135,7 +192,7 @@ function notgoing(){
     var curHtml = template(curData);
     //parentDiv.append(curHtml);
   })*/
-  
+
   // END - STEP 1
 
 
@@ -154,7 +211,7 @@ function notgoing(){
       localStorage.setItem('customName', newName);
     }
   });
-  */
+*/
   // END - STEP 2
 
 
